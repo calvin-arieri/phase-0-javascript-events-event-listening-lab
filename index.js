5beed1f41fdd63d04366d90a4823a282472841d9
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-13 08:43:04 +0300

    completed assignment.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function addingEventListener() {
-    const input = document.getElementById("button")
-    input.addEventListener('click', function(event) {
-        return "clicked"
-      });
+  const input = document.getElementById('button');
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
 }
+addingEventListener();
